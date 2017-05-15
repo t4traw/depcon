9f41f8b0810de55eed80a7eab798e5a60f190aab
t4traw <t4traw@gmail.com>
2017-05-15 14:31:31 +0900

    Add dependency charactor convertion method㊗️

--- lib/depcon.rb
+++ lib/depcon.rb
@@ -1,5 +1,26 @@
 require "depcon/version"
 
-module Depcon
-  # Your code goes here...
+require 'yaml'
+
+class String
+
+  def depcon
+    d = self.dup
+    convert_dependent_char(d)
+  end
+
+  def depcon!
+    convert_dependent_char(self)
+  end
+
+  private
+
+    def convert_dependent_char(str)
+      dic = YAML.load_file("#{File.dirname(__FILE__)}/dictionary.yml")
+      dic.each do |key, val|
+        str.gsub!(/#{key}/, val)
+      end
+      str
+    end
+
 end

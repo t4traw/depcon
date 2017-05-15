d10ec811954652ceb7c404a2f1d6febd3e32b2fb
t4traw <t4traw@gmail.com>
2017-05-15 14:57:07 +0900

    add test💯

--- test/depcon_test.rb
+++ test/depcon_test.rb
@@ -1,11 +1,26 @@
 require 'test_helper'
 
 class DepconTest < Minitest::Test
+  extend Minitest::Spec::DSL
+
+  let(:str) { "㈱サンプルから℡がありました。「さきほど送った1㌻の項目№③ですが、20㍉ではなく20㌢が正しい数値になります。修正お願いします。」" }
+  let(:expect_result) { "(株)サンプルからTELがありました。「さきほど送った1ページの項目No.(3)ですが、20ミリではなく20センチが正しい数値になります。修正お願いします。」" }
+
   def test_that_it_has_a_version_number
     refute_nil ::Depcon::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_depcon
+    assert_equal expect_result, str.depcon
+  end
+
+  def test_depcon_non_destructive_test
+    str.depcon
+    refute_equal expect_result, str
+  end
+
+  def test_depcon!
+    str.depcon!
+    assert_equal expect_result, str
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,5 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'depcon'
 
 require 'minitest/autorun'
+require "minitest/reporters"
+Minitest::Reporters.use!
